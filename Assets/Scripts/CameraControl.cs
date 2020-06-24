05b141d60454f7d76e14aad73cc5c2fb490bfe75
Woobinchan <48680923+Woobinchan@users.noreply.github.com>
2020-06-24 11:40:19 +0900

    Character,Camera Move implement
    캐릭터 애니메이션, 카메라의 캐릭터 추적, 캐릭터 이동 구현

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+
+{
+    public Transform player;
+    private Transform cameraTransform;
+    private float originalY;
+    Animator playerAnim;
+    Rigidbody2D playerRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = transform;
+        cameraTransform.position = new Vector3(player.position.x, player.position.y + 1.2f, cameraTransform.position.z);
+
+    }
+
+    private void Awake() {
+        playerAnim = GameObject.Find("Player").GetComponent<Animator>();
+        playerRigid = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer() {
+        originalY = this.transform.position.y;
+
+        if (cameraTransform.position.y - player.position.y > 1.2f) { //점프를 하던 말던 캐릭터가 카메라에서 너무 아래에 위치해있으면 카메라를 내림
+			cameraTransform.position = new Vector3(player.position.x, player.position.y + 1.2f, cameraTransform.position.z);
+			return;
+		}
+        if (player.position.y - cameraTransform.position.y > 1.2f) { //점프를 하던 말던 캐릭터가 카메라에서 너무 위에 위치해있으면 카메라를 올림
+            cameraTransform.position = new Vector3(player.position.x, originalY + 0.02f, cameraTransform.position.z);
+            return;
+        }
+
+
+        if (playerAnim.GetBool("isJumping")) { //뛰는중에는 카메라 y축 고정
+            cameraTransform.position = new Vector3(player.position.x, originalY, cameraTransform.position.z);
+            return;
+        } 
+        else { //not jumping
+            if ( cameraTransform.position.y - player.position.y < 1.2f) { //뛰어서 착지했을때 플레이어가 너무 높이있으면 카메라를 올림
+                cameraTransform.position = new Vector3(player.position.x, originalY + 0.02f, cameraTransform.position.z);
+                return;
+            }
+            else { 
+                    cameraTransform.position = new Vector3(player.position.x, originalY, cameraTransform.position.z);
+                    return;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -18,8 +18,10 @@ public class PlayerMove : MonoBehaviour
 
     void Update() {
         //jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping")) { 
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
 
 
         // Stop Speed
@@ -54,5 +56,14 @@ public class PlayerMove : MonoBehaviour
 		else if (rigid.velocity.x < maxSpeed * (-1)) {
 			rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
 		}
+        if (rigid.velocity.y < 0) {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null) {
+                if (rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping", false);
+            }
+        }
 	}
 }
